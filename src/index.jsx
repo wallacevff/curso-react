243d4371df2585afb449f897d5581c33da928500
Wallace Vidal <wallace.vidal@ipolitus.com>
2023-06-17 17:06:21 -0300

    Primeiro commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import "./index.css";
+import Primeiro from "./components/basicos/Primeiro";
+
+ReactDOM.render(
+    <div>
+        <Primeiro />
+    </div>,
+    document.getElementById('root')
+);
